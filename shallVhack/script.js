008e2302bcf66c76cf993c572c46e8f5102f7820
PratikDhumal-js <pratikdhumal2002@gmail.com>
2020-11-22 13:01:33 +0530

    updates

--- shallVhack/script.js
+++ shallVhack/script.js
@@ -1,32 +1,32 @@
-$(function() {
-    var text = $(".text");
-    $(window).scroll(function() {
-        var scroll = $(window).scrollTop();
+$(function () {
+  var text = $(".text");
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
 
-        if (scroll >= 200) {
-            text.removeClass("hidden");
-        } else {
-            text.addClass("hidden");
-        }
-    });
+    if (scroll >= 200) {
+      text.removeClass("hidden");
+    } else {
+      text.addClass("hidden");
+    }
+  });
 });
 
-var TxtType = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = "";
-    this.tick();
-    this.isDeleting = false;
+var TxtType = function (el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = "";
+  this.tick();
+  this.isDeleting = false;
 };
 
 AOS.init({
-    duration: 1200,
+  duration: 1200,
 });
 
-$(document).ready(function() {
-    new WOW().init();
+$(document).ready(function () {
+  new WOW().init();
 });
 
 // Trigger CSS animations on scroll.
@@ -36,101 +36,285 @@ $(document).ready(function() {
 // elements scroll below the fold again?
 // --> Check https://codepen.io/bramus/pen/vKpjNP
 
-jQuery(function($) {
-    // Function which adds the 'animated' class to any '.animatable' in view
-    var doAnimations = function() {
-        // Calc current offset and get all animatables
-        var offset = $(window).scrollTop() + $(window).height(),
-            $animatables = $(".animatable");
+jQuery(function ($) {
+  // Function which adds the 'animated' class to any '.animatable' in view
+  var doAnimations = function () {
+    // Calc current offset and get all animatables
+    var offset = $(window).scrollTop() + $(window).height(),
+      $animatables = $(".animatable");
 
-        // Unbind scroll handler if we have no animatables
-        if ($animatables.length == 0) {
-            $(window).off("scroll", doAnimations);
-        }
-
-        // Check all animatables and animate them if necessary
-        $animatables.each(function(i) {
-            var $animatable = $(this);
-            if ($animatable.offset().top + $animatable.height() - 20 < offset) {
-                $animatable.removeClass("animatable").addClass("animated");
-            }
-        });
-    };
-
-    // Hook doAnimations on scroll, and trigger a scroll
-    $(window).on("scroll", doAnimations);
-    $(window).trigger("scroll");
-});
-
-ocument.addEventListener("DOMContentLoaded", function(event) {
-    // array with texts to type in typewriter
-    var dataText = ["4 hours.", "4 hours", "4 hours.", "4 hours"];
-
-    // type one text in the typwriter
-    // keeps calling itself until the text is finished
-    function typeWriter(text, i, fnCallback) {
-        // chekc if text isn't finished yet
-        if (i < text.length) {
-            // add next character to h1
-            document.querySelector("h1").innerHTML =
-                text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
-
-            // wait for a while and call this function again for next character
-            setTimeout(function() {
-                typeWriter(text, i + 1, fnCallback);
-            }, 100);
-        }
-        // text finished, call callback if there is a callback function
-        else if (typeof fnCallback == "function") {
-            // call callback after timeout
-            setTimeout(fnCallback, 700);
-        }
+    // Unbind scroll handler if we have no animatables
+    if ($animatables.length == 0) {
+      $(window).off("scroll", doAnimations);
     }
-    // start a typewriter animation for a text in the dataText array
-    function StartTextAnimation(i) {
-        if (typeof dataText[i] == "undefined") {
-            setTimeout(function() {
-                StartTextAnimation(0);
-            }, 20000);
-        }
-        // check if dataText[i] exists
-        if (i < dataText[i].length) {
-            // text exists! start typewriter animation
-            typeWriter(dataText[i], 0, function() {
-                // after callback (and whole text has been animated), start next text
-                StartTextAnimation(i + 1);
-            });
-        }
-    }
-    // start the text animation
-    StartTextAnimation(0);
-});
 
-$(document).ready(function() {
-    // Add scrollspy to <body>
-    $("body").scrollspy({ target: ".navbar", offset: 50 });
-
-    // Add smooth scrolling on all links inside the navbar
-    $("#myNavbar a").on("click", function(event) {
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-            // Prevent default anchor click behavior
-            event.preventDefault();
-
-            // Store hash
-            var hash = this.hash;
-
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-            $("html, body").animate({
-                    scrollTop: $(hash).offset().top,
-                },
-                800,
-                function() {
-                    // Add hash (#) to URL when done scrolling (default click behavior)
-                    window.location.hash = hash;
-                }
-            );
-        } // End if
+    // Check all animatables and animate them if necessary
+    $animatables.each(function (i) {
+      var $animatable = $(this);
+      if ($animatable.offset().top + $animatable.height() - 20 < offset) {
+        $animatable.removeClass("animatable").addClass("animated");
+      }
     });
+  };
+
+  // Hook doAnimations on scroll, and trigger a scroll
+  $(window).on("scroll", doAnimations);
+  $(window).trigger("scroll");
+});
+
+ocument.addEventListener("DOMContentLoaded", function (event) {
+  // array with texts to type in typewriter
+  var dataText = ["4 hours.", "4 hours", "4 hours.", "4 hours"];
+
+  // type one text in the typwriter
+  // keeps calling itself until the text is finished
+  function typeWriter(text, i, fnCallback) {
+    // chekc if text isn't finished yet
+    if (i < text.length) {
+      // add next character to h1
+      document.querySelector("h1").innerHTML =
+        text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
+
+      // wait for a while and call this function again for next character
+      setTimeout(function () {
+        typeWriter(text, i + 1, fnCallback);
+      }, 100);
+    }
+    // text finished, call callback if there is a callback function
+    else if (typeof fnCallback == "function") {
+      // call callback after timeout
+      setTimeout(fnCallback, 700);
+    }
+  }
+  // start a typewriter animation for a text in the dataText array
+  function StartTextAnimation(i) {
+    if (typeof dataText[i] == "undefined") {
+      setTimeout(function () {
+        StartTextAnimation(0);
+      }, 20000);
+    }
+    // check if dataText[i] exists
+    if (i < dataText[i].length) {
+      // text exists! start typewriter animation
+      typeWriter(dataText[i], 0, function () {
+        // after callback (and whole text has been animated), start next text
+        StartTextAnimation(i + 1);
+      });
+    }
+  }
+  // start the text animation
+  StartTextAnimation(0);
+});
+
+$(document).ready(function () {
+  // Add scrollspy to <body>
+  $("body").scrollspy({ target: ".navbar", offset: 50 });
+
+  // Add smooth scrolling on all links inside the navbar
+  $("#myNavbar a").on("click", function (event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
+    } // End if
+  });
+});
+
+// @path-json can be an object or an array, the first will be loaded directly, the object from the array will be random selected
+/* tsParticles.loadJSON(@dom-id, @path-json, @callback (optional)); */
+
+tsParticles
+  .loadJSON("tsparticles", "presets/default.json")
+  .then((container) => {
+    console.log("callback - tsparticles config loaded");
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+//or
+
+/* tsParticles.load(@dom-id, @options); */
+
+tsParticles.load("tsparticles", {
+  /* options here */
+});
+
+//or
+
+/* tsParticles.loadFromArray(@dom-id, @options, @index (optional)); */
+
+tsParticles.loadFromArray("tsparticles", [
+  {
+    /* options here */
+  },
+  {
+    /* other options here */
+  },
+]);
+//random object
+
+tsParticles.loadFromArray(
+  "tsparticles",
+  [
+    {
+      /* options here */
+    },
+    {
+      /* other options here */
+    },
+  ],
+  1
+); //the second one
+// Important! If the index is not in range 0...<array.length, the index will be ignored.
+
+// after initialization this can be used.
+
+/* tsParticles.setOnClickHandler(@callback); */
+
+/* this will be fired from all particles loaded */
+
+tsParticles.setOnClickHandler((event, particles) => {
+  /* custom on click handler */
+});
+
+// now you can control the animations too, it's possible to pause and resume the animations
+// these methods don't change the config so you're safe with all your configurations
+// domItem(0) returns the first tsParticles instance loaded in the dom
+const particles = tsParticles.domItem(0);
+
+// play will start the animations, if the move is not enabled it won't enable it, it just updates the frame
+particles.play();
+
+// pause will stop the animations
+particles.pause();
+
+/* ---- tsParticles config ---- */
+
+tsParticles.load("tsparticles", {
+  fpsLimit: 60,
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+    color: {
+      value: ["#2EB67D", "#ECB22E", "#E01E5B", "#36C5F0"],
+    },
+    shape: {
+      type: ["circle"],
+      stroke: {
+        width: 0,
+        color: "#fff",
+      },
+      polygon: {
+        nb_sides: 5,
+      },
+      image: {
+        src: "https://cdn.freebiesupply.com/logos/large/2x/slack-logo-icon.png",
+        width: 100,
+        height: 100,
+      },
+    },
+    opacity: {
+      value: 1,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 8,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 10,
+        size_min: 10,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#808080",
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 5,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "grab",
+      },
+      onclick: {
+        enable: true,
+        mode: "push",
+      },
+      resize: true,
+    },
+    modes: {
+      grab: {
+        distance: 140,
+        line_linked: {
+          opacity: 1,
+        },
+      },
+      bubble: {
+        distance: 400,
+        size: 40,
+        duration: 2,
+        opacity: 8,
+        speed: 3,
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4,
+      },
+      push: {
+        particles_nb: 4,
+      },
+      remove: {
+        particles_nb: 2,
+      },
+    },
+  },
+  retina_detect: true,
 });
