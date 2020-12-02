9b94177a40ef3668061642a09241e6e4a6e2b151
PratikDhumal-js <pratikdhumal2002@gmail.com>
2020-12-03 02:53:11 +0530

    updates

--- shallVhack/app.js
+++ shallVhack/app.js
@@ -131,3 +131,14 @@ particlesJS(
     },
   }
 );
+
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".carousel");
+  var instances = M.Carousel.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function () {
+  $(".carousel").carousel();
+});
--- shallVhack/script.js
+++ shallVhack/script.js
@@ -318,3 +318,37 @@ tsParticles.load("tsparticles", {
   },
   retina_detect: true,
 });
+
+var swiper = new Swiper(".swiper-container", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 20,
+    stretch: 0,
+    depth: 200,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+(function (i, s, o, g, r, a, m) {
+  i["GoogleAnalyticsObject"] = r;
+  (i[r] =
+    i[r] ||
+    function () {
+      (i[r].q = i[r].q || []).push(arguments);
+    }),
+    (i[r].l = 1 * new Date());
+  (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m);
+})(window, document, "script", "//www.google-analytics.com/analytics.js", "ga");
+
+ga("create", "UA-46156385-1", "cssscript.com");
+ga("send", "pageview");
