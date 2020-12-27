ea9e91eb15c27b9b8769137cf02d9b743655b9f8
PratikDhumal-js <pratikdhumal2002@gmail.com>
2020-12-27 17:11:15 +0530

    updates

--- shallVhack/script.js
+++ shallVhack/script.js
@@ -1656,3 +1656,106 @@ $(document).ready(function () {
   });
 });
 z;
+
+window.addEventListener("load", function () {
+  document
+    .querySelector(".glider")
+    .addEventListener("glider-slide-visible", function (event) {
+      var glider = Glider(this);
+      console.log("Slide Visible %s", event.detail.slide);
+    });
+  document
+    .querySelector(".glider")
+    .addEventListener("glider-slide-hidden", function (event) {
+      console.log("Slide Hidden %s", event.detail.slide);
+    });
+  document
+    .querySelector(".glider")
+    .addEventListener("glider-refresh", function (event) {
+      console.log("Refresh");
+    });
+  document
+    .querySelector(".glider")
+    .addEventListener("glider-loaded", function (event) {
+      console.log("Loaded");
+    });
+
+  window._ = new Glider(document.querySelector(".glider"), {
+    slidesToShow: 1, //'auto',
+    slidesToScroll: 1,
+    itemWidth: 150,
+    draggable: true,
+    scrollLock: false,
+    dots: "#dots",
+    rewind: true,
+    arrows: {
+      prev: ".glider-prev",
+      next: ".glider-next",
+    },
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToScroll: "auto",
+          itemWidth: 300,
+          slidesToShow: "auto",
+          exactWidth: true,
+        },
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToScroll: 4,
+          slidesToShow: 4,
+          dots: false,
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToScroll: 3,
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToScroll: 2,
+          slidesToShow: 2,
+          dots: false,
+          arrows: false,
+          scrollLock: true,
+        },
+      },
+    ],
+  });
+});
+
+var swiper = new Swiper(".blog-slider", {
+  spaceBetween: 30,
+  effect: "fade",
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  // autoHeight: true,
+  pagination: {
+    el: ".blog-slider__pagination",
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper(".blog-slider", {
+  spaceBetween: 30,
+  effect: "fade",
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  // autoHeight: true,
+  pagination: {
+    el: ".blog-slider__pagination",
+    clickable: true,
+  },
+});
